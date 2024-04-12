bcd2780887267af2900eff7ff2016d2232903073
Champagnepapi <yanngaudin11@yahoo.com>
2024-04-12 11:08:33 +0200

    test

---
+++ travailEnModeProje.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon site simple</title>
+</head>
+<body>
+    <h1>Bienvenue sur mon site simple en PHP !</h1>
+
+    <?php
+    // Code PHP ici
+    ?>
+
+</body>
+</html>
